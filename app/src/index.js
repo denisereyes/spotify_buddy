23175e8467a1a878a88cec1fb15e12e8014df7ef
denisereyes <dreyes12@binghamton.edu>
2023-10-13 13:03:55 -0400

    added support for firebase

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+
+//firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6dyCVe2J-afssXECMkP2OpbyOGLOlck4",
+  authDomain: "spotify-wrapped-ca411.firebaseapp.com",
+  projectId: "spotify-wrapped-ca411",
+  storageBucket: "spotify-wrapped-ca411.appspot.com",
+  messagingSenderId: "201725267224",
+  appId: "1:201725267224:web:193f0474500af3bda7226d",
+  measurementId: "G-192SFM5CG5"
+}; 
+
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
